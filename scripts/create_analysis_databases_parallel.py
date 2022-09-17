b5ec6b30a6a2c522ac9f3c49cc359e19e46074c6
Tim Blazytko <tim@blazytko.to>
2022-09-17 17:21:41 +0200

    added helper script to analyze a folder of binaries in parallel

---
+++ scripts/create_analysis_databases_parallel.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+import glob
+import sys
+import os
+import multiprocessing
+
+from binaryninja import BinaryViewType
+
+
+def analyze_file(file_path):
+    print(f"Analyzing file {file_path}.")
+
+    # analyze with binaryninja
+    bv = BinaryViewType.get_view_of_file(file_path)
+    bv.update_analysis_and_wait()
+
+    # save analysis database in filesystem
+    bv.create_database(f"{file_path}.bndb")
+
+    # delete for memory optimization
+    del bv
+
+    return file_path
+
+
+# check file arguments
+if len(sys.argv) < 2:
+    print("[*] Syntax: {} <path to analysis directory>".format(sys.argv[0]))
+    exit(0)
+
+# parse arguments
+analysis_directory = sys.argv[1]
+
+# analyze files in parallel
+with multiprocessing.Pool() as pool:
+    mapping = pool.map(analyze_file,  [file_path for file_path in glob.glob(
+        f"{analysis_directory}/*") if not file_path.endswith(".bndb") and not os.path.exists(file_path + ".bndb")])
