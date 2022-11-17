6ff4b1f6fc403a5267ee4ff69143d3e71a66e7dc
Tim Blazytko <tim@blazytko.to>
2022-11-17 22:34:33 +0100

    added python main function wrapper for script

--- scripts/create_analysis_databases_parallel.py
+++ scripts/create_analysis_databases_parallel.py
@@ -23,15 +23,16 @@ def analyze_file(file_path):
     return file_path
 
 
-# check file arguments
-if len(sys.argv) < 2:
-    print("[*] Syntax: {} <path to analysis directory>".format(sys.argv[0]))
-    exit(0)
+if __name__ == '__main__':
+    # check file arguments
+    if len(sys.argv) < 2:
+        print("[*] Syntax: {} <path to analysis directory>".format(sys.argv[0]))
+        exit(0)
 
-# parse arguments
-analysis_directory = sys.argv[1]
+    # parse arguments
+    analysis_directory = sys.argv[1]
 
-# analyze files in parallel
-with multiprocessing.Pool() as pool:
-    mapping = pool.map(analyze_file,  [file_path for file_path in glob.glob(
-        f"{analysis_directory}/*") if not file_path.endswith(".bndb") and not os.path.exists(file_path + ".bndb")])
+    # analyze files in parallel
+    with multiprocessing.Pool() as pool:
+        mapping = pool.map(analyze_file, [file_path for file_path in glob.glob(
+            f"{analysis_directory}/*") if not file_path.endswith(".bndb") and not os.path.exists(file_path + ".bndb")])
